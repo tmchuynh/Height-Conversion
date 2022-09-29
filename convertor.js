0d33ffa97d030fd1f8272eb5b99cdd161d2fa8f1
Tina Huynh <tinamchuynh@gmail.com>
2022-09-29 11:42:42 -0700

    Conversion

---
+++ convertor.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+    let feet = document.querySelector('#feet');
+    let inches = document.querySelector('#inches');
+    const results = document.querySelector('#results');
+    e.preventDefault();
+
+    feet = parseInt(feet.value);
+    inches = parseInt(inches.value);
+
+    if (isNaN(feet) || isNaN(inches)) {
+        results.textContent = "Please enter a valid number!";
+    } else if (feet < 0) {
+        results.textContent = "Please enter a feet value > 0";
+    } else if (inches < 0 || inches >= 12) {
+        results.textContent = "Please enter an inch value between 0 and 12";
+    } else {
+        //cm = inches * 2.54
+        let totalInches = (feet * 12) + inches;
+        results.textContent = `${totalInches} cm`;
+        document.querySelector('#feet').value = '';
+        document.querySelector('#inches').value = '';
+    }
+})
